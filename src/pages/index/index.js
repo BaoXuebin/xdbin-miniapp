7e316048d1f481ef6469c309081de4347d3f553f
leo <leo.bao@dyuanzi.com>
2019-02-01 15:34:29 +0800

    修改样式

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -17,43 +17,68 @@ class Index extends Component {
                 key: '1',
                 day: '27',
                 month: '一月',
-                content: '必胜客吃披萨',
-                images: [
-                  'http://cdn.xdbin.com/foods/tmp_186c956716919ac75d1b1e5901e031d4bee68e38e404b625.jpg',
-                  'http://cdn.xdbin.com/foods/tmp_654a0de06c3dbe27f4fe660ae8f99e3371a505f04ce43ee4.jpg',
-                  'http://cdn.xdbin.com/foods/tmp_a1219f6050ab60ddcaa637bf11cbc78802d5afe22479854f.jpg',
-                  'http://cdn.xdbin.com/foods/tmp_b07481e2621617ca3ebd4c8f1b49139f32c4c0ed977b667d.jpg',
-                  'http://cdn.xdbin.com/pics/20190122214909'
-                ],
-                publishTime: new Date()
+                notes: [
+                    {
+                        key: 'note_1',
+                        content: '必胜客吃披萨',
+                        images: [
+                            'http://cdn.xdbin.com/foods/tmp_186c956716919ac75d1b1e5901e031d4bee68e38e404b625.jpg',
+                            'http://cdn.xdbin.com/foods/tmp_654a0de06c3dbe27f4fe660ae8f99e3371a505f04ce43ee4.jpg',
+                            'http://cdn.xdbin.com/foods/tmp_a1219f6050ab60ddcaa637bf11cbc78802d5afe22479854f.jpg',
+                            'http://cdn.xdbin.com/foods/tmp_b07481e2621617ca3ebd4c8f1b49139f32c4c0ed977b667d.jpg'
+                        ],
+                        publishTime: new Date()
+                    },
+                    {
+                        key: 'note_2',
+                        content: '必胜客吃披萨',
+                        images: [
+                            'http://cdn.xdbin.com/foods/tmp_186c956716919ac75d1b1e5901e031d4bee68e38e404b625.jpg',
+                            'http://cdn.xdbin.com/foods/tmp_654a0de06c3dbe27f4fe660ae8f99e3371a505f04ce43ee4.jpg',
+                            'http://cdn.xdbin.com/foods/tmp_a1219f6050ab60ddcaa637bf11cbc78802d5afe22479854f.jpg',
+                            'http://cdn.xdbin.com/foods/tmp_b07481e2621617ca3ebd4c8f1b49139f32c4c0ed977b667d.jpg'
+                        ],
+                        publishTime: new Date()
+                    },
+                ]
             },
             {
                 key: '2',
                 day: '08',
                 month: '一月',
-                content: '伊曼努尔·康德，著名德意志哲学家，德国古典哲学创始人，启蒙运动时期最重要的思想家之一。生于东普鲁士哥尼斯堡（今俄罗斯加里宁格勒）。哥尼斯堡大学毕业。1755年起在母校执教，1770年升教授。其思想分为“前批判时期”和“批判时期”。在前批判时期，以自然科学的研究为主，并进行哲学探究。1755年发表《自然通史和天体论》，提出关于太阳系起源的星云假说。其学说深深影响近代西方哲学，并开启了德国唯心主义和康德主义等诸多流派。',
-                images: [
-                    'http://cdn.xdbin.com/pics/20190122214909'
+                notes: [
+                    {
+                        key: 'note_1',
+                        content: '伊曼努尔·康德，著名德意志哲学家，德国古典哲学创始人，启蒙运动时期最重要的思想家之一。生于东普鲁士哥尼斯堡（今俄罗斯加里宁格勒）。哥尼斯堡大学毕业。1755年起在母校执教，1770年升教授。其思想分为“前批判时期”和“批判时期”。在前批判时期，以自然科学的研究为主，并进行哲学探究。1755年发表《自然通史和天体论》，提出关于太阳系起源的星云假说。其学说深深影响近代西方哲学，并开启了德国唯心主义和康德主义等诸多流派。',
+                        images: [
+                            'http://cdn.xdbin.com/pics/20190122214909'
+                        ],
+                        publishTime: new Date()
+                    },
+                    {
+                        key: 'note_2',
+                        content: '伊曼努尔·康德，著名德意志哲学家，德国古典哲学创始人，启蒙运动时期最重要的思想家之一。生于东普鲁士哥尼斯堡（今俄罗斯加里宁格勒）。哥尼斯堡大学毕业。1755年起在母校执教，1770年升教授。其思想分为“前批判时期”和“批判时期”。在前批判时期，以自然科学的研究为主，并进行哲学探究。1755年发表《自然通史和天体论》，提出关于太阳系起源的星云假说。其学说深深影响近代西方哲学，并开启了德国唯心主义和康德主义等诸多流派。',
+                        images: [
+                            'http://cdn.xdbin.com/pics/20190122214909'
+                        ],
+                        publishTime: new Date()
+                    }
                 ],
                 publishTime: new Date()
             },
             {
                 key: '3',
-                day: '30',
+                day: '23',
                 month: '十二',
-                content: '伊曼努尔·康德，著名德意志哲学家，德国古典哲学创始人，启蒙运动时期最重要的思想家之一。生于东普鲁士哥尼斯堡（今俄罗斯加里宁格勒）。哥尼斯堡大学毕业。1755年起在母校执教，1770年升教授。其思想分为“前批判时期”和“批判时期”。在前批判时期，以自然科学的研究为主，并进行哲学探究。1755年发表《自然通史和天体论》，提出关于太阳系起源的星云假说。其学说深深影响近代西方哲学，并开启了德国唯心主义和康德主义等诸多流派。',
-                images: [
-                    'http://cdn.xdbin.com/pics/20190122214909'
-                ],
-                publishTime: new Date()
-            },
-            {
-                key: '4',
-                day: '17',
-                month: '十二',
-                content: '伊曼努尔·康德，著名德意志哲学家，德国古典哲学创始人，启蒙运动时期最重要的思想家之一。生于东普鲁士哥尼斯堡（今俄罗斯加里宁格勒）。哥尼斯堡大学毕业。1755年起在母校执教，1770年升教授。其思想分为“前批判时期”和“批判时期”。在前批判时期，以自然科学的研究为主，并进行哲学探究。1755年发表《自然通史和天体论》，提出关于太阳系起源的星云假说。其学说深深影响近代西方哲学，并开启了德国唯心主义和康德主义等诸多流派。',
-                images: [
-                    'http://cdn.xdbin.com/pics/20190122214909'
+                notes: [
+                    {
+                        key: 'note_1',
+                        content: '伊曼努尔·康德，著名德意志哲学家',
+                        images: [
+                            'http://cdn.xdbin.com/foods/tmp_a1219f6050ab60ddcaa637bf11cbc78802d5afe22479854f.jpg'
+                        ],
+                        publishTime: new Date()
+                    }
                 ],
                 publishTime: new Date()
             }
--- src/pages/index/item/timeline_item.js
+++ src/pages/index/item/timeline_item.js
@@ -5,13 +5,7 @@ import monment from 'moment';
 import './timeline_item.scss';
 class TimelineItem extends Component {
 
-    readImage(event) {
-        console.log(event);
-        let idx = event;
-        if (typeof idx !== 'number') {
-            idx = event.target.dataset.eReadimageAA;
-        }
-        const { images } = this.props.body;
+    readImage(idx, images) {
         wx.previewImage({
             current: images[idx],
             urls: images
@@ -19,37 +13,58 @@ class TimelineItem extends Component {
     }
 
     render() {
-        const { day, month, content, images, publishTime } = this.props.body;
+        const { day, month, notes } = this.props.body;
         const last = this.props.last;
         return (
             <View className={last ? "timeline-item timeline-item-last" : "timeline-item"}>
                 <View className="timeline-item-tail"></View>
                 <View className="timeline-item-head time-line-item-head-blue"></View>
-                <View className="time-line-date">
-                    <Text className="day">{day}</Text>
-                    <Text className="month">{month}</Text>
-                </View>
-                <View className="timeline-item-content">
-                    {content}
-                    <View className='content-container'>
-                        {
-                            (images && images.length > 0) &&
-                            <View>
-                                {images.map((i, idx) => (
-                                    <Image key={i} onClick={this.readImage.bind(this, idx)} lazyLoad class='thumbnail' src={i} alt="图片" />
-                                ))}
+                {
+                    notes.map((note, idx) => (
+                        <View key={note.key} className="timeline-item-layout">
+                            <View className="timeline-item-left">
+                                {
+                                    idx === 0 &&
+                                    <View className="timeline-item-left-1 time-line-date">
+                                        <Text className="day">{day}</Text>
+                                        <Text className="month">{month}</Text>
+                                    </View>
+                                }
+                                <View className="timeline-item-left-2 ">
+                                    <View className="timeline-item-avatar">
+                                        <Image lazyLoad style={{ width: '100%', height: '100%' }} src="http://cdn.xdbin.com/pics/20190122214909" alt="图片" />
+                                    </View>
+                                </View>
                             </View>
-                        }
-                        <View className="meta-container">
-                            <View className="meta-time">
-                                {monment(publishTime).format('HH:mm')}
-                            </View>
-                            <View className="meta-img-num">
-                                {images ? `共${images.length}张` : ''}
+                            <View className="timeline-item-right">
+                                <View key={note.key} className="timeline-item-content">
+                                    <View>
+                                        <View className="timeline-item-author">@初意</View>
+                                        {note.content}
+                                        <View className="content-container">
+                                            {
+                                                (note.images && note.images.length > 0) &&
+                                                <View>
+                                                    {note.images.map((i, idx) => (
+                                                        <Image key={i} onClick={this.readImage.bind(this, idx, note.images)} lazyLoad class='thumbnail' src={i} alt="图片" />
+                                                    ))}
+                                                </View>
+                                            }
+                                            <View className="meta-container">
+                                                <View className="meta-time">
+                                                    {monment(note.publishTime).format('HH:mm')}
+                                                </View>
+                                                <View className="meta-img-num">
+                                                    {note.images ? `共${note.images.length}张` : ''}
+                                                </View>
+                                            </View>
+                                        </View>
+                                    </View>
+                                </View>
                             </View>
                         </View>
-                    </View>
-                </View>
+                    ))
+                }
             </View>
         );
     }
